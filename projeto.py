3d187abc935b654d48c932245012369f5dc4ad74
Hugo Santos <hugojogos3@gmail.com>
2023-03-17 02:32:44 -0300

    Adicionando Canvas

--- projeto.py
+++ projeto.py
@@ -16,14 +16,16 @@ imgbuttoncancel = b'iVBORw0KGgoAAAANSUhEUgAAAMgAAAAvCAMAAACRx3WWAAAAAXNSR0IB2cks
 pergunta_atual = 0
 # Tudo que tiver dentro da janela
 layout =[   
-            [sg.Text(perguntas.perguntas[pergunta_atual]['pergunta'], font=('Consolas', 20), text_color='white', size=(50, None))],
+            [sg.Text(perguntas.perguntas[pergunta_atual]['pergunta'], font=('Consolas', 20), text_color='white', size=(sizetxt, None))],
+            [sg.Canvas(size=(750,2), background_color='white')],
+            [sg.Canvas(size=(0,10))],
             [sg.Radio(f"A) {perguntas.perguntas[pergunta_atual]['opcoes'][0]}", font=('Calibri', 15), group_id='fala', size=(sizetxt, None))],
             [sg.Radio(f"B) {perguntas.perguntas[pergunta_atual]['opcoes'][1]}", font=('Calibri', 15), group_id='fala', size=(sizetxt, None))],
             [sg.Radio(f"C) {perguntas.perguntas[pergunta_atual]['opcoes'][2]}", font=('Calibri', 15), group_id='fala', size=(sizetxt, None))],
             [sg.Radio(f"D) {perguntas.perguntas[pergunta_atual]['opcoes'][3]}", font=('Calibri', 15), group_id='fala', size=(sizetxt, None))],
             [sg.Text('', key='msg')],
-            [sg.Button('   ', image_data=imgbuttonprox, button_color=(sg.theme_background_color(),sg.theme_background_color()), border_width=0),
-            sg.Button('  ', image_data=imgbuttoncancel, button_color=(sg.theme_background_color(),sg.theme_background_color()), border_width=0)]
+            [sg.Button('', image_data=imgbuttonprox, button_color=(sg.theme_background_color(),sg.theme_background_color()), border_width=0, key='Proximo'),
+            sg.Button('', image_data=imgbuttoncancel, button_color=(sg.theme_background_color(),sg.theme_background_color()), border_width=0, key='Cancelar')]
         ]
 
 # Cria a Janela
@@ -33,54 +35,56 @@ janela = sg.Window('Janela teste', layout)
 while True:
     event, values = janela.read()
     #se o usuário fechar ou cancelar
-    if event == sg.WIN_CLOSED or event == '  ':
+    if event == sg.WIN_CLOSED or event == 'Cancelar':
         break
 
-    if event == '   ':
+    if event == 'Proximo':
         if values[0]:
             if perguntas.perguntas[pergunta_atual]['resp'] != perguntas.perguntas[pergunta_atual]['opcoes'][0]:
-                sg.popup(f'Resposta incorreta. A resposta correta era: {perguntas.perguntas[pergunta_atual]["resp"]}')
+                sg.popup(f'Resposta incorreta.\nA resposta correta era: {perguntas.perguntas[pergunta_atual]["resp"]}', font=('Calibri', 15))
             else:
-                sg.popup('Resposta correta!')
+                sg.popup('Resposta correta!', font=('Calibri', 15))
                 pontos += 1
 
             
         elif values[1]:
             if perguntas.perguntas[pergunta_atual]['resp'] != perguntas.perguntas[pergunta_atual]['opcoes'][1]:
-                sg.popup(f'Resposta incorreta. A resposta correta era: {perguntas.perguntas[pergunta_atual]["resp"]}')
+                sg.popup(f'Resposta incorreta.\nA resposta correta era: {perguntas.perguntas[pergunta_atual]["resp"]}', font=('Calibri', 15))
             else:
-                sg.popup('Resposta correta!')
+                sg.popup('Resposta correta!', font=('Calibri', 15))
                 pontos += 1
 
         elif values[2]:
             if perguntas.perguntas[pergunta_atual]['resp'] != perguntas.perguntas[pergunta_atual]['opcoes'][2]:
-                sg.popup(f'Resposta incorreta. A resposta correta era: {perguntas.perguntas[pergunta_atual]["resp"]}')
+                sg.popup(f'Resposta incorreta.\nA resposta correta era: {perguntas.perguntas[pergunta_atual]["resp"]}', font=('Calibri', 15))
             else:
-                sg.popup('Resposta correta!')
+                sg.popup('Resposta correta!', font=('Calibri', 15))
                 pontos += 1
         
         elif values[3]:
             if perguntas.perguntas[pergunta_atual]['resp'] != perguntas.perguntas[pergunta_atual]['opcoes'][3]:
-                sg.popup(f'Resposta incorreta. A resposta correta era: {perguntas.perguntas[pergunta_atual]["resp"]}')
+                sg.popup(f'Resposta incorreta.\nA resposta correta era: {perguntas.perguntas[pergunta_atual]["resp"]}', font=('Calibri', 15))
             else:
-                sg.popup('Resposta correta!')
+                sg.popup('Resposta correta!', font=('Calibri', 15))
                 pontos += 1
 
         pergunta_atual += 1
 
         if pergunta_atual == len(perguntas.perguntas):
-                sg.popup('Fim do jogo!')
+                sg.popup('Fim do jogo!', font=('Calibri', 15))
                 break
 
         layout =[   
-            [sg.Text(perguntas.perguntas[pergunta_atual]['pergunta'], font=('Consolas', 20), text_color='white', size=(50, None))],
+            [sg.Text(perguntas.perguntas[pergunta_atual]['pergunta'], font=('Consolas', 20), text_color='white', size=(sizetxt, None))],
+            [sg.Canvas(size=(750,2), background_color='white')],
+            [sg.Canvas(size=(0,10))],
             [sg.Radio(f"A) {perguntas.perguntas[pergunta_atual]['opcoes'][0]}", font=('Calibri', 15), group_id='fala', size=(sizetxt, None))],
             [sg.Radio(f"B) {perguntas.perguntas[pergunta_atual]['opcoes'][1]}", font=('Calibri', 15), group_id='fala', size=(sizetxt, None))],
             [sg.Radio(f"C) {perguntas.perguntas[pergunta_atual]['opcoes'][2]}", font=('Calibri', 15), group_id='fala', size=(sizetxt, None))],
             [sg.Radio(f"D) {perguntas.perguntas[pergunta_atual]['opcoes'][3]}", font=('Calibri', 15), group_id='fala', size=(sizetxt, None))],
             [sg.Text('', key='msg')],
-            [sg.Button('   ', image_data=imgbuttonprox, button_color=(sg.theme_background_color(),sg.theme_background_color()), border_width=0),
-            sg.Button('  ', image_data=imgbuttoncancel, button_color=(sg.theme_background_color(),sg.theme_background_color()), border_width=0)]
+            [sg.Button('', image_data=imgbuttonprox, button_color=(sg.theme_background_color(),sg.theme_background_color()), border_width=0, key='Proximo'),
+            sg.Button('', image_data=imgbuttoncancel, button_color=(sg.theme_background_color(),sg.theme_background_color()), border_width=0, key='Cancelar')]
             ]
         janela.close()
         janela = sg.Window('Janela teste', layout)
